40d808518b9545baf8ea80b987418785419f6112
zeusgolohor <zeusgolohor@gmail.com>
2024-05-16 02:33:18 +0000

    Redis only allows to store string, bytes and numbers (and lists thereof). Whatever you store as single elements, it will be returned as a byte string. Hence if you store a as a UTF-8 string, it will be returned as ba when retrieved from the server.
    In this exercise we will create a get method that take a key string argument and an optional Callable argument named fn. This callable will be used to convert the data back to the desired format.
    
    Remember to conserve the original Redis.get behavior if the key does not exist.
    
    Also, implement 2 new methods: get_str and get_int that will automatically parametrize Cache.get with the correct conversion function.
    
    The following code should not raise:

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,7 +6,7 @@ private variable named _redis
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -27,3 +27,31 @@ class Cache:
         rid = str(uuid.uuid4())
         self._redis.set(rid, data)
         return (rid)
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """
+        A method used to get values from the db.
+        """
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """
+        A method to convert get to str.
+        """
+        value = self._redis.get(key)
+        return value.decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """
+        A method to convert get to int
+        """
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode("utf-8"))
+        except Exception as e:
+            value = 0
+        return value
